3973ced53179463e297c6115ce5f918ede31d01c
ramonestii <ramonestii@hotmail.com>
2014-10-27 02:30:45 -0200

    Atualizaçao Classe PortaAviao
    Atualizacao dos Arquivos

--- Program_Navio/PortaAviao.cpp
+++ Program_Navio/PortaAviao.cpp
@@ -9,3 +9,11 @@ PortaAviao::PortaAviao(const PortaAviao& orig) {
 PortaAviao::~PortaAviao() {
 }
 
+void setqtdAviao(int qtdAviao){
+    this->qtdAviao = qtdAviao;
+}
+
+int getqtdAviao(){
+    return qtdAviao;
+}
+
